c0e4173395be679d66be755d9ab1f205b62f229c
Juuso Lehtinen <jupelehtinen@gmail.com>
2011-08-02 17:15:29 +0300

    Paranneltu ehdotusten mukaisesti

--- app/actions/laskut.php
+++ app/actions/laskut.php
@@ -1,14 +1,10 @@
 <?php
 
-//Haetaan maksamattomat varaukset
-$haku = Atomik_Db::query("SELECT COUNT(DISTINCT varaaja) FROM varaukset WHERE maksettu = 'false'");
-$haku->execute();
-$luku = $haku->fetch();
 
-if($luku > 0) { //Jos maksamattomia varauksia on ollut, niin haetaan käyttäjät, joita ei laskutettu
-    $haku = Atomik_Db::query("SELECT DISTINCT varaaja FROM varaukset WHERE maksettu = 'false'");
-    $haku->execute();
-}
+//Jos maksamattomia varauksia on ollut, niin haetaan käyttäjät, joita ei laskutettu
+$haku = Atomik_Db::query("SELECT DISTINCT varaaja FROM varaukset WHERE maksettu = ?", array('false'));
+$haku->execute();
+
 
 //Haetaan laskut
 $laskut = Atomik_Db::findAll('laskut');
