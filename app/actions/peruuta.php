b1c103e8940e8af06af9dce08dca0c00d75064b0
Juuso Lehtinen <jupelehtinen@gmail.com>
2011-08-12 20:03:41 +0300

    laskutettua varausta ei voi en perua

--- app/actions/peruuta.php
+++ app/actions/peruuta.php
@@ -13,12 +13,14 @@ if(($data = Atomik::filter($_GET, $rule)) === false){ //Parametrien suodatus
     return;
 }
 
+//Haetaan poistettavan varauksen tiedot
 $haku = Atomik_Db::query("SELECT * FROM varaukset WHERE tunniste = ?", array($data['tunniste']));
 $haku->execute();
 $poistettava = $haku->fetch();
 
-//Testit: Poistettava varaus on oma, varausta ei voi poistaa samana päivänä
-if($poistettava['varaaja'] != Atomik::get('session/user') || $poistettava['pvm'] <= time()){
+//Testit: Poistettava varaus on oma, varausta ei voi poistaa samana päivänä, varaus on laskutettu
+if($poistettava['varaaja'] != Atomik::get('session/user') || $poistettava['pvm'] <= time() 
+        || $poistettava['maksettu']){
     Atomik::flash('Peruutus ei onnistunut', 'error');
     Atomik::redirect('index');
     return;
